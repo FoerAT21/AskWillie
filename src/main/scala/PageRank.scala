3fc977e2a5c259cd9de3e6d313a578aca6091b26
FOERSTAT21 <FoerstAt21@gcc.edu>
2025-03-28 11:38:50 -0400

    got indegree working and fixes equal

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -1,5 +1,5 @@
 import scala.util.Random
-// import scala.collection.parallel.CollectionConverters._
+//import scala.collection.parallel.CollectionConverters._
 
 object PageRank {
     /**
@@ -7,15 +7,22 @@ object PageRank {
      * @return      A map of page.id to a weight of 1.0 for those same WebPage objects
      */
     def equal(pages: Map[String, WebPage]): Map[String, Double] = {
-        for (s, wp) <- pages yield (s, 1.0) // correct??
+        for(key <- pages.keys) do {
+            println(key)
+        }
+        for(key <- pages.keys) do {
+            println("Links " + pages(key).links)
+        }
+        pages.map((id, page) => (id,1.0))
     }
 
     /**
      * @param pages A map of page.id to page for some number of WebPage objects
-     * @return A map of page.id to a weight that is a simple count of the number of pages linking to that page
+     * @return A map of page.id to a weight that is a simple count of the number
+     *         of pages linking to that page
      */
     def indegree(pages: Map[String, WebPage]): Map[String, Double] = {
-        Map()
+        pages.map((id,_) => (id, pages.map((_,page) => page.links.count(_ contains id)).sum.toDouble))
     }
 
     def pagerank(pages: Map[String, WebPage]): Map[String, Double] = {
