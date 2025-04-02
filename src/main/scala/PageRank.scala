d45fba3bcc9cb813e25ffe48766a4044f5706bf2
FOERSTAT21 <FoerstAt21@gcc.edu>
2025-04-01 21:31:50 -0400

    pagerank

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.util.Random
 //import scala.collection.parallel.CollectionConverters._
 
@@ -20,6 +21,37 @@ object PageRank {
     }
 
     def pagerank(pages: Map[String, WebPage]): Map[String, Double] = {
-        Map() // TODO: remove this stub and implement this method
+        val S = 10000
+        val N = pages.size
+
+        def combineMaps(map1 : Map[String, Double], map2: Map[String, Double]): Map[String, Double] = {
+            map1.map((id , count) => (id, count+map2(id)))
+        }
+
+        val finalCounts = (0 until S).map { _ =>
+            val startingPage = pages.keys.toSeq(Random.nextInt(pages.size))
+            walker(pages, startingPage, pages.map((id, _) => (id, 0.0)))
+        }.foldLeft(pages.map((id, _) => (id, 0.0)))(combineMaps)
+
+        finalCounts.map((id, count) => (id, count+1/(S+N).toDouble))
+    }
+
+    @tailrec
+    def walker(pages : Map[String, WebPage], currPageId : String, currMap : Map[String,Double], clicks : Int = 0): Map[String,Double] = {
+        if clicks == 100 then currMap
+        else {
+            val currentPage: WebPage = pages(currPageId)
+            val newPage =
+                if (Random.nextDouble() <= 0.85 && currentPage.links.nonEmpty)
+                    currentPage.links(Random.nextInt(currentPage.links.length))
+                else
+                    pages.keys.toSeq(Random.nextInt(pages.size))
+
+            walker(pages, newPage, followLink(newPage, currMap), clicks + 1)
+        }
+    }
+
+    def followLink(pageid : String, currMap: Map[String,Double]): Map[String, Double] = {
+        currMap.map((id,count) => if pageid != id then (id,count) else (id,count+1.0))
     }
 }
